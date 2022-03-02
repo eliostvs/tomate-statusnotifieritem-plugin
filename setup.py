2b890c2d806a89b94ecfe726e7c0557bcbf251cf
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2022-03-01 22:19:35 -0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     name="tomate_statusnotifieritem-plugin",
     data_files=find_data_files(DATA_FILES, "tomate"),
     url="https://github.com/eliostvs/tomate-statusnotifieritem-plugin",
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
 )
